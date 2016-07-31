fadd5e80591c011444d3679f2e3d965fc81aaf8d
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-07-31 20:19:14 +0300

    refactor program Viselitsa with using classes

---
+++ viselitsa2.rb
@@ -0,0 +1,10 @@
+require "./game.rb"
+require "./result_printer.rb"
+require "./graphic_method.rb"
+
+printer = ResultPrinter.new
+#В ResultPrinter нет метода initialize, 
+# значит нет никакого особого конструктора
+#и чтобы создать обьект нужно вызвать оператор .new
+
+slovo = ARGV[0]
