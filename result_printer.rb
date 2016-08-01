787559e5a2d9c7abff9bd2d08c509d95ab5ad2da
Sergiy Dmytrenko <dmytrenko.serhii@gmail.com>
2016-08-01 22:50:08 +0300

    Create Class ResultPrinter

--- result_printer.rb
+++ result_printer.rb
@@ -1,11 +1,41 @@
 # Печать результатов на экран. Он выводит состояние 
 # игры на экран
 class ResultPrinter
+	def print_status(game) #передаем клас игры
 
+		clear_screen
 
+		puts "\nСлово: " + get_word_for_print(game.secret_word, game.good_letters)
+		puts "Ошибки (#{game.errors}): #{game.bad_letters.join(", ")}"
 
+		print_viselitsa(game.errors)
 
+		if game.errors >= 7
+			puts puts "Вы проиграли :("
+		else
+			if game.secret_word.uniq.size == game.good_letters.size
+				puts "\n\nПоздравляем! Вы выиграли!\n\n"
+			else
+				puts "У вас осталось попыток: " + (7-game.errors).to_s
+			end
+		end
+	end
 
+	def get_word_for_print(secret_word, good_letters)
+		result = ""
 
+		for letter in secret_word do
+			if good_letters.include? letter
+				result += letter + " "
+			else
+				result += "__ "
+			end
+		end
 
+		return result
+	end
+
+	def clear_screen
+		system "clear"
+	end
 end
